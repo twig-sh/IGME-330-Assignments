313d2ac9604ee44abeb5330f7f2d22a1c6c9a031
twig-sh <sheherlisc@gmail.com>
2024-04-04 10:45:05 -0400

    more react

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
 import "./styles.css";
 
 const App = () => {
-  return "Hello World";
+  const [newItem, setNewItem] = useState("test");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // this adds a new todo object to the end of the array
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false
+        }
+      ]
+    });
+    setNewItem(""); //clear out input
+  };
+
+  return (
+    <>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+
+      <h1 className="header">Todo List</h1>
+
+      <ul className="list">
+        {todos.map(todo => {
+          return <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        })}
+      </ul>
+    </>
+  )
 };
 
 export default App;
